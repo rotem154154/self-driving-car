884dea5c27a17af4be718212a6a96bb4bca84191
rotem israeli <irotem98@gmail.com>
2019-02-21 06:46:11 +0200

    added score system

--- Car_Map.py
+++ Car_Map.py
@@ -6,16 +6,29 @@ class CarMap:
                    289, 401, 207, 431, 130, 469, 110, 424, 128, 320, 157, 229, 95, 99, 217, 87]
     self.out_map = [344, 89, 445, 45, 572, 59, 691, 92, 756, 229, 695, 333, 715, 482, 649, 566, 455, 567, 409, 502, 319,
                     476, 214, 492, 125, 550, 78, 557, 45, 485, 41, 353, 87, 254, 38, 88, 74, 35, 271, 44, 295, 70]
-    self.score_lines = [341, 161,344, 89]
+    self.score_lines = [341, 161,344, 89,567, 123,572, 59,678, 241,756, 229,654, 467,715, 482,506, 514,455, 567,130, 469,125, 550,157, 229,87, 254,95, 99,74, 35]
     self.in_batch = pyglet.graphics.Batch()
     self.out_batch = pyglet.graphics.Batch()
     self.in_grey = [160] * (len(self.in_map) / 2)
     self.out_grey = [160] * (len(self.out_map) / 2)
+    self.blue1 = [0, 39, 102,1]*(len(self.score_lines)/2)
+    self.blue2 = [0, 97, 255, 1] * (2)
     self.in_batch.add(len(self.in_map) / 2, pyglet.gl.GL_LINE_LOOP, None, ('v2i', self.in_map),
                       ('c4B', self.in_grey * 4))
     self.out_batch.add(len(self.out_map) / 2, pyglet.gl.GL_LINE_LOOP, None, ('v2i', self.out_map),
                        ('c4B', self.out_grey * 4))
 
-  def draw(self):
+  def draw(self,score_activate):
     self.in_batch.draw()
     self.out_batch.draw()
+    score_batch = pyglet.graphics.Batch()
+    score_batch.add(16,pyglet.gl.GL_LINES,None,('v2i',self.score_lines),('c4B',self.blue1))
+    score_batch.draw()
+    score_batch2 = pyglet.graphics.Batch()
+    score_batch2.add(2, pyglet.gl.GL_LINES, None, ('v2i', self.score_points(score_activate)), ('c4B', self.blue2))
+    score_batch2.draw()
+
+  def score_points(self,score_activate):
+    if score_activate == 7:
+      score_activate = -1
+    return self.score_lines[score_activate*4+4:score_activate*4+8]
--- main.py
+++ main.py
@@ -9,6 +9,11 @@ from pyglet.window import mouse
 red = [255, 0, 0, 0]
 green = [0, 255, 0, 0]
 window = pyglet.window.Window(width=800,height=600)
+label = pyglet.text.Label(str(0),
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center')
 
 @window.event
 def on_mouse_press(x, y, button, modifiers):
@@ -26,8 +31,20 @@ def update_frames(dt):
     car.update(keys)
     if collision.car_map_collision(car.get_points(),map):
         car.red_bool = True
+        car.score -= 10
     else:
         car.red_bool = False
+
+    score_points = map.score_points((car.last_score + 1) % 8)
+    if collision.car_line_collision(car.get_points(),score_points[0],score_points[1],score_points[2],score_points[3]):
+        car.score += 500
+        car.last_score += 1
+
+
+
+
+
+    car.score -= 1
 
 
 @window.event
@@ -35,8 +52,10 @@ def on_draw():
     pyglet.gl.glClearColor(0,0,0,0)
     window.clear()
     pyglet.gl.glClearColor(0, 50, 0, 0)
-    map.draw()
+    map.draw(score_activate=(car.last_score+1)%8)
     car.draw()
+    label.text = str(car.score)
+    label.draw()
 
 map = CarMap()
 
